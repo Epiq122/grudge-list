079a88896a265f78f097bb0cebe11e6c90f18a18
Robert Gleason <l2ob122@gmail.com>
2024-03-05 21:53:14 -0800

    Add cache revalidation and create Grudges table

--- app/actions/create-grudge.js
+++ app/actions/create-grudge.js
@@ -1,12 +1,14 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
 
 export async function addGrudge(formData) {
   const name = formData.get('grudgeName');
   try {
     if (!name) throw new Error('Name is required');
     await sql`INSERT INTO Grudges (Name) VALUES (${name})`;
+    revalidatePath('/');
   } catch (error) {
     return { status: 500 };
   }
